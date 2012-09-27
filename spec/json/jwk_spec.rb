6e6c1c39db331c259dbce1f5c1bc00b24034ebfb
nov matake <nov@matake.jp>
2012-09-27 10:38:05 +0900

    pem difference between 1.9.2 and 1.9.3 seems OK for JWK usage

--- spec/json/jwk_spec.rb
+++ spec/json/jwk_spec.rb
@@ -66,15 +66,16 @@ describe JSON::JWK do
     context 'when RSA' do
       subject do
         JSON::JWK.decode(
-          alg: :RSA,
-          mod: mod,
-          exp: exp
+          :alg => :RSA,
+          :mod => mod,
+          :exp => exp
         )
       end
       let(:exp) { 'AQAB' }
       let(:mod) { 'AK8ppaAGn6N3jDic2DhDN5mI5mWzvhfL1AFZOS9q2EBM8L5sjZbYiaHeNoKillZGmEF9a9g6Z20bDnoHTuHPsx93HYkZqPumFZ8K9lLCbqKAMWw2Qgk10RgrZ-kblJotTBCeer9-tZSWO-OWFzP4gp8MpSuQOQbwTJwDgEkFIQLUK2YgzWbn1PoW8xcfbVyWhZD880ELGRW6GhRgYAl0DN_EQS8kyUa0CusYCzOOg2W3-7qjYeojyP6jiOEr-eyjC7hcUvTVoTfz84BiZv72KS3i5JS8ZNNuRp5Ce51wjoDDUoNxDLWv6Da6qMaGpKz6NTSNbvhE_KFhpp4wf5yRQD8=' }
       let(:pem) do
-        <<-PEM
+        if RUBY_VERSION >= '1.9.3'
+          <<-PEM
 -----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArymloAafo3eMOJzYOEM3
 mYjmZbO+F8vUAVk5L2rYQEzwvmyNltiJod42gqKWVkaYQX1r2DpnbRsOegdO4c+z
@@ -84,7 +85,19 @@ nwylK5A5BvBMnAOASQUhAtQrZiDNZufU+hbzFx9tXJaFkPzzQQsZFboaFGBgCXQM
 LeLklLxk025GnkJ7nXCOgMNSg3EMta/oNrqoxoakrPo1NI1u+ET8oWGmnjB/nJFA
 PwIDAQAB
 -----END PUBLIC KEY-----
-        PEM
+          PEM
+        else
+          <<-PEM
+-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEArymloAafo3eMOJzYOEM3mYjmZbO+F8vUAVk5L2rYQEzwvmyNltiJ
+od42gqKWVkaYQX1r2DpnbRsOegdO4c+zH3cdiRmo+6YVnwr2UsJuooAxbDZCCTXR
+GCtn6RuUmi1MEJ56v361lJY745YXM/iCnwylK5A5BvBMnAOASQUhAtQrZiDNZufU
++hbzFx9tXJaFkPzzQQsZFboaFGBgCXQM38RBLyTJRrQK6xgLM46DZbf7uqNh6iPI
+/qOI4Sv57KMLuFxS9NWhN/PzgGJm/vYpLeLklLxk025GnkJ7nXCOgMNSg3EMta/o
+NrqoxoakrPo1NI1u+ET8oWGmnjB/nJFAPwIDAQAB
+-----END RSA PUBLIC KEY-----
+          PEM
+        end
       end
 
       it { should be_instance_of OpenSSL::PKey::RSA }
