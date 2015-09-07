4464ca14030c379acd5c71c00966d79c4ca18e21
nov <nov@matake.jp>
2015-09-07 13:56:13 +0900

    nimbus interop test fixed

--- spec/interop/with_nimbus_jose_spec.rb
+++ spec/interop/with_nimbus_jose_spec.rb
@@ -14,7 +14,7 @@ describe 'interop' do
 
               it 'should decryptable by Nimbus JOSE JWT' do
                 jwe.encrypt! key
-                NimbusJWE.decrypt(jwe, private_key_path).should == plain_text
+                NimbusJWE.decrypt(jwe, private_key_path).should == input.to_json
               end
             end
 
@@ -23,7 +23,7 @@ describe 'interop' do
 
               it 'should decryptable by Nimbus JOSE JWT' do
                 jwe.encrypt! key
-                NimbusJWE.decrypt(jwe, private_key_path).should == plain_text
+                NimbusJWE.decrypt(jwe, private_key_path).should == input.to_json
               end
             end
           end
@@ -34,7 +34,7 @@ describe 'interop' do
 
               it 'should decryptable by Nimbus JOSE JWT' do
                 jwe.encrypt! key
-                NimbusJWE.decrypt(jwe, private_key_path).should == plain_text
+                NimbusJWE.decrypt(jwe, private_key_path).should == input.to_json
               end
             end
 
@@ -43,14 +43,16 @@ describe 'interop' do
 
               it 'should decryptable by Nimbus JOSE JWT' do
                 jwe.encrypt! key
-                NimbusJWE.decrypt(jwe, private_key_path).should == plain_text
+                NimbusJWE.decrypt(jwe, private_key_path).should == input.to_json
               end
             end
           end
 
-          context 'when plaintext given' do
-            let(:plain_text) { 'Hello World' }
-            let(:jwe) { JSON::JWE.new plain_text }
+          context 'when Hash given' do
+            let(:input) do
+              {foo: :bar}
+            end
+            let(:jwe) { JSON::JWE.new input }
 
             context 'when alg=RSA1_5' do
               let(:key) { public_key }
@@ -70,9 +72,8 @@ describe 'interop' do
           end
 
           context 'when jwt given' do
-            let(:plain_text) { jwt.to_s }
-            let(:jwt) { JSON::JWT.new(foo: :bar) }
-            let(:jwe) { JSON::JWE.new jwt }
+            let(:input) { JSON::JWT.new(foo: :bar) }
+            let(:jwe) { JSON::JWE.new input }
 
             context 'when alg=RSA-OAEP' do
               let(:key) { public_key }
