42dce26442e944097ec7ac9ffc02af94a61fafb6
nov <nov@matake.jp>
2016-01-26 13:42:34 +0900

    alias_method_chain wasn't needed here at all

--- lib/json/jws.rb
+++ lib/json/jws.rb
@@ -27,15 +27,14 @@ module JSON
       end
     end
 
-    def update_with_jose_attributes(hash_or_jwt)
-      update_without_jose_attributes hash_or_jwt
+    def update(hash_or_jwt)
+      super
       if hash_or_jwt.is_a? JSON::JWT
         self.header = hash_or_jwt.header
         self.signature = hash_or_jwt.signature
       end
       self
     end
-    alias_method_chain :update, :jose_attributes
 
     private
 
